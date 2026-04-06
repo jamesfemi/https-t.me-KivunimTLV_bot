41ae55bc5b695283f06d9ab4ede85f94aebab619
jamesfemi <jamesfemi323@gmail.com>
2026-04-06 18:12:50 +0100

    Refactor bot to use environment variable for BOT_TOKEN
    Updated bot to use environment variable for token and changed command handling.

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
+import os
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 
-# Your bot token from BotFather
-BOT_TOKEN = "8497214557:AAHPXpdcquwy8NCWllBpvgRY1nzj5fWEpKg"
+BOT_TOKEN = os.getenv("8497214557:AAHPXpdcquwy8NCWllBpvgRY1nzj5fWEpKg")
+
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN missing!")
 
 WELCOME_MESSAGE = """מדויקת לפי הצרכים שלך 🌿
 
@@ -15,17 +18,18 @@ https://t.me/m/lreqLDxBYjA0
 👆👆להזמנות וקישקושים לחצו על הקישורים מחכים לכם 👆👆
 """
 
-# Command: /start
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+# Reply to ANY message
+async def auto_reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print("Message received")
     await update.message.reply_text(WELCOME_MESSAGE)
 
 def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
-    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, auto_reply))
 
     print("Bot is running...")
-    app.run_polling()
+    app.run_polling(drop_pending_updates=True)
 
 if __name__ == "__main__":
     main()
